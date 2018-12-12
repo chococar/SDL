963d4a77f94773f8de544ebf718fa5b98f051263
Xavier Leng <180161T@mymail.nyp.edu.sg>
2018-12-12 13:15:38 +0800

    Xavier was here

---
+++ Persistence.py
@@ -0,0 +1,38 @@
+import shelve as s
+
+class User:
+    def __init__(self, name, password):
+        self.__name = name
+        self.__password = password
+
+users = s.open('user')
+
+def clear_user():
+    klist = list(users.keys())
+    for key in klist:
+        del users[key]
+
+def get_users():
+    user_list = []
+    klist = list(users.keys())
+    for key in klist:
+        user_list.append(users[key])
+    return user_list
+
+def init_users():
+    clear_user()
+    for i in range(5):
+        create_user('user'+str(i), 'pass'+str(i))
+
+def get_user(username, password):
+    if username in users:
+        user = users[username]
+        if username.password == password:
+            return user
+    return None
+
+def create_user(username, password):
+    u = User()
+    u.name = username
+    u.password = password
+    users[username] = u
--- app.py
+++ app.py
@@ -1,23 +1,47 @@
 from flask import Flask, render_template
+from Persistence import *
 app = Flask(__name__)
 
 @app.route("/")
-def mainpage():
+def main():
     return render_template("Main Page.html")
 
 
-@app.route("/Login Page")
-def loginpage():
-    return render_template("Login_Page.html")
+@app.route("/login", methods=('GET', 'POST'))
+def login():
+    login_form = LoginForm(request.form)
+    error = None
+    if request.method == 'POST':
+        user = get_user(login_form.id.data, login_form.password.data)
+        if user is None:
+            error = 'Wrong username and password'
+        else:
+            session['username'] = user.username
+            return redirect (url_for('index'))
+        flash(error)
+    return render_template("Login_Page.html", form=login_form)
 
 
-@app.route("/Register Page")
-def registerpage():
-    return render_template("Register.html")
+@app.route("/register", methods=('GET', 'POST'))
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        username = form.id.data
+        password = form.password.data
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            create_user(username, password)
+            return redirect (url_for('login'))
+        flash(error)
+    return render_template("Register.html", form=form)
 
 
-@app.route("/Reminder Page")
-def reminderpage():
+@app.route("/remind")
+def reminder():
     return render_template("Reminder page.html")
 
 
@@ -40,13 +64,16 @@ def cctv2():
 def cctv3():
     return render_template("CCTV_3.html")
 
+
 @app.route("/CCTV_4")
 def cctv4():
     return render_template("CCTV_4.html")
 
+
 @app.route("/Aircon")
 def aircon():
     return render_template("AirconDesign.html")
+
 
 @app.route("/Lighting")
 def lighting():
@@ -57,14 +84,21 @@ def lighting():
 def index():
     return render_template('tracker.html')
 
+
 @app.route('/items')
 def items():
     return render_template('items.html')
+
 
 @app.route('/history')
 def history():
     return render_template('history.html')
 
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return login()
 
 if __name__ == "__main__":
     app.run()
